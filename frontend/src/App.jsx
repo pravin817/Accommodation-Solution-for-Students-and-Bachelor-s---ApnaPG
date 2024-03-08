da8efa2cb32800e73cca34d8b963ad737c9e44d1
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-03-08 18:40:26 +0530

    Added : react-router-dom, react-hot-toast configured successfully.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,31 @@
 import "./App.css";
+import toast, { Toaster } from "react-hot-toast";
+import { RouterProvider } from "react-router-dom";
+
+// import the router
+import router from "./Routes/Route";
+
+const notify = () => {
+  toast("Welcome  !", {
+    icon: "👏",
+  });
+  console.log("notify clicked");
+};
 
 function App() {
   return (
     <>
+      {/* Dummy check for the tailwind and toast  */}
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button onClick={notify} className="btn">
+        Make me Toast
+      </button>
+
+      {/* wrapup the router  */}
+      <RouterProvider router={router} />
+
+      {/* Add for the Toast Notification  */}
+      <Toaster></Toaster>
     </>
   );
 }
---
+++ frontend/src/Components/Shared/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link to="/">
+        <p>Home</p>
+      </Link>
+
+      <Link to="/about">
+        <p>About</p>
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
