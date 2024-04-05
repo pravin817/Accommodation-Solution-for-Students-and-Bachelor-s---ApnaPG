aaaa72b8faca5e65335c4ef1532e722c6ab72223
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-04-05 10:06:46 +0530

    Feature : Redux toolkit - roomReducer and roomAction added successfully.

--- frontend/src/redux/reducers/rootReducer.js
+++ frontend/src/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import handleUser from "../reducers/userReducer";
+import roomReducer from "./roomReducer";
 
 const rootReducer = combineReducers({
   user: handleUser,
+  room: roomReducer,
 });
 
 export default rootReducer;
